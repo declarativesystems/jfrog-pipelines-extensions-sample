80da4e03f64da8884a701ebdbaa9357006ecd74d
Geoff Williams <geoff@declarativesystems.com>
2020-10-23 08:16:42 +1100

    steal d env

--- steps/declarativesystems/nodejs/onExecute.sh
+++ steps/declarativesystems/nodejs/onExecute.sh
@@ -1,5 +1,17 @@
+getArtifactoryVariable() {
+  local artifactoryName=$1
+  local variable=$2
+  echo eval $(int_$(artifactoryName)_$(variable))
+}
+
 checkHealth() {
   local success=true
+
+  local artifactoryName=$(find_step_configuration_value "sourceArtifactory")
+  local artifactoryUser=$(getArtifactoryVariable "$artifactoryName" "name")
+  local artifactoryId=$(getArtifactoryVariable "$artifactoryName" "id")
+  local artifactoryUrl=$(getArtifactoryVariable "$artifactoryName" "url")
+  env
   echo "yooooorrrr the best!!!!!!~~~~111"
   npm config set registry https://declarativesystems.jfrog.io/artifactory/api/npm/npm/
 #  local url=$(find_step_configuration_value "healthCheckUrl")
@@ -8,7 +20,7 @@ checkHealth() {
 #  } || exitCode=$?
 #  if test $statusCode -ne 200; then
 #    export success=false
-#    echo "Health check failed with statusCode: $statusCode & exitCode: $exitCode for url: $url"
+#    echo "Health check failed with statusCode: $senmvtatusCode & exitCode: $exitCode for url: $url"
 #  else
 #    echo "Health check succeeded"
 #  fi
