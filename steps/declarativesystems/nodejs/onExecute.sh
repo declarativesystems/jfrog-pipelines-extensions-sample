10befd5667a984630988c89e10adb0b65de4e6e8
Geoff Williams <geoff@declarativesystems.com>
2020-10-22 21:21:09 +1100

    test

---
+++ steps/declarativesystems/nodejs/onExecute.sh
@@ -0,0 +1,19 @@
+checkHealth() {
+  local success=true
+  echo "yooooorrrr the best!!!!!!~~~~111"
+  npm config set registry https://declarativesystems.jfrog.io/artifactory/api/npm/npm/
+#  local url=$(find_step_configuration_value "healthCheckUrl")
+#  {
+#    local statusCode=$(curl --silent --output /dev/stderr --write-out "%{http_code}" "$url")
+#  } || exitCode=$?
+#  if test $statusCode -ne 200; then
+#    export success=false
+#    echo "Health check failed with statusCode: $statusCode & exitCode: $exitCode for url: $url"
+#  else
+#    echo "Health check succeeded"
+#  fi
+
+  $success
+}
+
+execute_command checkHealth
